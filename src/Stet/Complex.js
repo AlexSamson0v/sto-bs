971da4f46bb66cf7b04573449f2926dfb3d8083d
Alexandr Samsonov <8semalli8@gmail.com>
2023-05-21 13:35:00 +0300

    1

--- src/Stet/Complex.js
+++ src/Stet/Complex.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 export const Complex = () => (
   <h1> Page Carousel</h1>  
 )
--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -4,7 +4,7 @@ import car2 from "../img/car2.jpg";
 import car3 from "../img/car3.jpg";
 import car4 from "../img/car4.jpg";
 import car5 from "../img/car5.jpg";
-
+import React from 'react';
 function UncontrolledExample() {
   return (
     <Carousel>
