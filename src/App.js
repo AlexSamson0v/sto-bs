0a194e01ef93a6e4108896f2238500b512e1b031
Alexandr Samsonov <8semalli8@gmail.com>
2023-05-25 15:58:19 +0300

    созданы страницы

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import  NavBar  from './components/Navbar';
 import  Carousel  from './components/Carousel';
 import  Card  from './components/Cards/Card/Card';
 import { Routes, Route} from "react-router-dom";
-import {Record} from './Stet/Record';
-import {Ditaling} from './Stet/Ditaling';
-import {Usual} from './Stet/Usual'
-import {Scleaner} from './Stet/Scleaner';
-import {Complex} from './Stet/Complex';
-import {Regular} from './Stet/Regular';
+import Record from './Conteiners/RecordPage/Record';
+import {Ditaling} from './Conteiners/DitailingPage/Ditaling';
+import {Usual} from './Conteiners/UsualPage/Usual'
+import {Scleaner} from './Conteiners/SclinerPage/Scleaner';
+import {Complex} from './Conteiners/ComplexPage/Complex';
+import {Regular} from './Conteiners/RegularPage/Regular';
 import { Navbar } from 'react-bootstrap';
 import Login from './components/Login';
 
@@ -21,12 +21,12 @@ function App() {
    <Card/>
    <Routes> 
    <Route path='/' element={<Navbar/>} />
-    <Route path='/Stet/Record' element={<Record/>} />
-    <Route path='/Stet/Usual' element={<Usual/>} />
-    <Route path='/Stet/Ditaling' element={<Ditaling/>} />
-    <Route path='/Stet/Scleaner' element={<Scleaner/>} />
-    <Route path='/Stet/Complex' element={<Complex/>} />
-    <Route path='/Stet/Regular' element={<Regular/>} />
+    <Route path='/Conteiners/RecordPage/Record' element={<Record/>} />
+    <Route path='/Conteiners/UsualPage/Usual' element={<Usual/>} />
+    <Route path='/Conteiners/DitailingPage/Ditaling' element={<Ditaling/>} />
+    <Route path='/Conteiners/SclinerPage/Scleaner' element={<Scleaner/>} />
+    <Route path='/Conteiners/ComplexPage/Complex' element={<Complex/>} />
+    <Route path='/Conteiners/RegularPage/Regular' element={<Regular/>} />
     <Route path='/components/Login' element={<Login/>} />
 
    </Routes> 
--- src/Stet/Record.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const Record = () => (
-  <h1> PageRecord</h1>  
-)
